b649c525bebf272df9f694529a5b4fd7525f89c0
linxOD <linxOD@users.noreply.github.com>
2024-02-06 16:19:17 +0000

    deploy: 7600d1bb8d089cefa391f628dc4d38e391a21cf1

--- js/search.js
+++ js/search.js
@@ -1,10 +1,37 @@
 var project_collection_name = "gestrich_index"
 const annoUrl = "https://anno.onb.ac.at/cgi-content/anno?aid=wrz&datum="
+const annoImgUrl = "https://anno.onb.ac.at/cgi-content/annoshow?call=wrz|"
 
 function makeAnnoLink(hit) {
     let hitDate = hit.day;
     let hitPage = hit.page;
-    return `${annoUrl}${hitDate}&seite=${hitPage}`
+    let hitDigit = hit.digitarium_issue;
+    let hidId = hit.id;
+    let hitGestrich = hit.gestrich;
+    if (hitDigit) {
+        let wr_app_id = hidId.split('__')[0].replace('wr_', '');
+        return `${wr_app_id}.html#wr_page_${hitPage}`;
+    } else if (hitGestrich) {
+        let wr_app_id = hidId.split('__')[0];
+        return `${wr_app_id}.html#wr_page_${hitPage}`;
+    } else {
+        return `${annoUrl}${hitDate}&seite=${hitPage}`;
+    }
+}
+function makeAnnoImgUrl(hit) {
+    let hitDate = hit.day;
+    let hitPage = hit.page;
+    return `${annoImgUrl}${hitDate}|${hitPage}|10.0|0`;
+}
+function makeTitle(hit) {
+    let hitDigit = hit.digitarium_issue;
+    let hitGestrich = hit.gestrich;
+    if (hitDigit) {
+        var title = hit.title.split(', ')[0];
+    } else if (hitGestrich) {
+        var title = hit.title.split(', ')[2];
+    }
+    return title;
 }
 const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
     server: {
@@ -26,6 +53,7 @@ const searchClient = typesenseInstantsearchAdapter.searchClient;
 const search = instantsearch({
     searchClient,
     indexName: project_collection_name,
+    routing: true,
 });
 
 search.addWidgets([
@@ -43,25 +71,74 @@ search.addWidgets([
     instantsearch.widgets.hits({
         container: "#hits",
         cssClasses: {
-            item: "w-100"
+            item: "w-20 border border-light rounded m-2 p-2 d-flex flex-column",
         },
         templates: {
             empty: "Keine Resultate für <q>{{ query }}</q>",
             item(hit, { html, components }) {
+                console.log(hit);
                 return html` 
-            <h3><a href='${makeAnnoLink(hit)}'>${hit.title}</a></h3>
-            <p>${hit._snippetResult.full_text.matchedWords.length > 0 ? components.Snippet({ hit, attribute: 'full_text' }) : ''}</p>
-            <p>${hit.places.map((item) => html`<a href='${item.id}'><span class="badge rounded-pill m-1 bg-info">${item}</span></a>`)}
-            ${hit.keywords.map((item) => html`<a href='${item.id}'><span class="badge rounded-pill m-1 bg-success">${item}</span></a>`)}</p>
-            <p><small>sonstige Schlagworte:</small> ${hit.extra_full_text}</p>
+                <a class="text-decoration-none text-dark" 
+                    href='${makeAnnoLink(hit)}'
+                    aria-label="Link zu Dokument: ${hit.title}">
+                    <div class="row align-items-center">
+                        <div class="col-md-4">
+                            <img src="${makeAnnoImgUrl(hit)}" class="img-thumbnail" alt="Bild"/>
+                        </div>
+                        <div class="col-md-8">
+                            <table class="table table-sm">
+                                <tr>
+                                    <td><strong>Ausgabe:</strong></td>
+                                    <td>${makeTitle(hit)}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>ID:</strong></td>
+                                    <td>${hit.id}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Artikel:</strong></td>
+                                    <td>${hit.article_count}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Seite:</strong></td>
+                                    <td>${hit.page}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Jahr:</strong></td>
+                                    <td>${hit.year}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Edition:</strong></td>
+                                    <td>${hit.edition[0]}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Korrekturstatus:</strong></td>
+                                    <td>${hit.corrections}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </a>
             `;
             },
         },
     }),
 
+    // <p>${hit._snippetResult.full_text.matchedWords.length > 0 ? components.Snippet({ hit, attribute: 'full_text' }) : ''}</p>
+    // <p>${hit.places.map((item) => html`<a href='${item.id}'><span class="badge rounded-pill m-1 bg-info">${item}</span></a>`)}
+    // ${hit.keywords.map((item) => html`<a href='${item.id}'><span class="badge rounded-pill m-1 bg-success">${item}</span></a>`)}</p>
+    // <p><small>sonstige Schlagworte:</small> ${hit.extra_full_text}</p>
+
     instantsearch.widgets.pagination({
         container: "#pagination",
     }),
+
+    instantsearch.widgets.menu({
+        container: "#menu-edition",
+        attribute: "edition",
+    }),
+
+
 
     instantsearch.widgets.stats({
         container: "#stats-container",
@@ -84,6 +161,21 @@ search.addWidgets([
     }),
 
     instantsearch.widgets.refinementList({
+        container: "#corrections",
+        attribute: "corrections",
+        searchable: false,
+        cssClasses: {
+            searchableInput: "form-control form-control-sm m-2 border-light-2",
+            searchableSubmit: "d-none",
+            searchableReset: "d-none",
+            showMore: "btn btn-secondary btn-sm align-content-center",
+            list: "list-unstyled",
+            count: "badge d-flex align-self-end m-2 badge-secondary hideme ",
+            label: "d-flex align-items-start text-left text-capitalize",
+            checkbox: "m-2",
+        },
+    }),
+    instantsearch.widgets.refinementList({
         container: "#has-fulltext",
         attribute: "has_fulltext",
         searchable: false,
@@ -93,8 +185,8 @@ search.addWidgets([
             searchableReset: "d-none",
             showMore: "btn btn-secondary btn-sm align-content-center",
             list: "list-unstyled",
-            count: "badge m-2 badge-secondary hideme ",
-            label: "d-flex align-items-center text-capitalize",
+            count: "badge d-flex align-self-end m-2 badge-secondary hideme ",
+            label: "d-flex align-items-start text-left text-capitalize",
             checkbox: "m-2",
         },
     }),
@@ -108,8 +200,8 @@ search.addWidgets([
             searchableReset: "d-none",
             showMore: "btn btn-secondary btn-sm align-content-center",
             list: "list-unstyled",
-            count: "badge m-2 badge-secondary hideme ",
-            label: "d-flex align-items-center text-capitalize",
+            count: "badge d-flex align-self-end m-2 badge-secondary hideme ",
+            label: "d-flex align-items-start text-left text-capitalize",
             checkbox: "m-2",
         },
     }),
@@ -123,8 +215,8 @@ search.addWidgets([
             searchableReset: "d-none",
             showMore: "btn btn-secondary btn-sm align-content-center",
             list: "list-unstyled",
-            count: "badge m-2 badge-secondary hideme ",
-            label: "d-flex align-items-center text-capitalize",
+            count: "badge d-flex align-self-end m-2 badge-secondary hideme ",
+            label: "d-flex align-items-start text-left text-capitalize",
             checkbox: "m-2",
         },
     }),
@@ -140,8 +232,8 @@ search.addWidgets([
             searchableReset: "d-none",
             showMore: "btn btn-secondary btn-sm align-content-center",
             list: "list-unstyled",
-            count: "badge m-2 badge-secondary hideme ",
-            label: "d-flex align-items-center text-capitalize",
+            count: "badge d-flex align-self-end m-2 badge-secondary hideme",
+            label: "d-flex align-items-start text-left text-capitalize",
             checkbox: "m-2",
         },
     }),
@@ -166,8 +258,8 @@ search.addWidgets([
         cssClasses: {
             showMore: "btn btn-secondary btn-sm align-content-center",
             list: "list-unstyled",
-            count: "badge m-2 badge-secondary hideme",
-            label: "d-flex align-items-center",
+            count: "badge d-flex align-self-end m-2 badge-secondary hideme",
+            label: "d-flex align-items-start text-left",
             checkbox: "m-2",
         },
     }),
@@ -231,7 +323,17 @@ search.addWidgets([
         container: "#pagination",
         padding: 2,
         cssClasses: {
-            list: "pagination",
+            list: "pagination my-4",
+            item: "page-item",
+            link: "page-link",
+        },
+    }),
+
+    instantsearch.widgets.pagination({
+        container: "#pagination0",
+        padding: 2,
+        cssClasses: {
+            list: "pagination my-4",
             item: "page-item",
             link: "page-link",
         },
