497bedfb32749e27e4f274db20bca60725dae7d9
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2024-06-19 12:37:43 +0200

    updating baseurl logic; removing keystatic hybrid settings for main branch

--- astro.config.ts
+++ astro.config.ts
@@ -2,17 +2,10 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import keystatic from "@keystatic/astro";
+// import vercel from "@astrojs/vercel/serverless";
+// import keystatic from "@keystatic/astro";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
-const prodUrl = "https://digitarium.acdh-dev.oeaw.ac.at";
-const devUrl = "https://curved-conjunction.vercel.app";
-const prodBase = prodUrl.includes("https://digitarium.acdh-dev.oeaw.ac.at")
-	? "/"
-	: "/curved-conjunction";
-const devBase = "/";
 
 // https://astro.build/config
 export default defineConfig({
@@ -36,19 +29,17 @@ export default defineConfig({
 			},
 		}),
 		react(),
-		...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()]),
 		tailwind(),
 		mdx(),
 	],
 	build: {
 		format: "file",
 	},
-	output: process.env.SKIP_KEYSTATIC ? "static" : "hybrid",
+	output: "static",
 	// output: "static",
 	server: {
 		port: 3000,
 	},
-	site: process.env.SKIP_KEYSTATIC ? prodUrl : devUrl,
-	base: process.env.SKIP_KEYSTATIC ? prodBase : devBase,
-	adapter: process.env.SKIP_KEYSTATIC ? undefined : vercel(),
+	site: "https://wiener-diarium.github.io/wr-app",
+	// base: "/wr-app/",
 });
---
+++ src/lib/baseurl.ts
@@ -0,0 +1,5 @@
+export const baseurl = "/";
+
+export const createBaseurl = (url: string | undefined) => {
+	return baseurl + url;
+};
